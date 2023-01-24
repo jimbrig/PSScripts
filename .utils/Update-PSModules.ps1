32d18bc574d8fc51b4ac378d5ad0d6951be68298
Jimmy Briggs <jimmy.briggs@pwc.com>
2023-01-24 13:58:28 -0500

    feat: add .utils scripts

---
+++ .utils/Update-PSModules.ps1
@@ -0,0 +1,34 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 82faa144-6d74-49de-b9b7-e036a91275e1
+
+.AUTHOR Jimmy Briggs
+
+.COMPANYNAME jimbrig
+
+.COPYRIGHT Jimmy Briggs | 2023
+
+.TAGS PowerShell Modules Management Utility Update Cleanup
+
+.LICENSEURI https://github.com/jimbrig/PSScripts/blob/main/LICENSE
+
+.PROJECTURI https://github.com/jimbrig/PSScripts/Update-PSModules/
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+Initial Release
+
+.PRIVATEDATA
+
+#>
+
