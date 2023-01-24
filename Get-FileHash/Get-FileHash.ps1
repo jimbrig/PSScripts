15263caa557774766540e52ae542cf6d54f6c566
Jimmy Briggs <jimmy.briggs@pwc.com>
2023-01-24 14:44:02 -0500

    feat: skaffold out new Get-FileHash script

---
+++ Get-FileHash/Get-FileHash.ps1
@@ -0,0 +1,43 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 114aa534-9b99-4f6b-8e36-d57f51374f98
+
+.AUTHOR Jimmy Briggs
+
+.COMPANYNAME jimbrig
+
+.COPYRIGHT Jimmy Briggs | 2023
+
+.TAGS Files MD5 Hash Serialization Utility
+
+.LICENSEURI https://github.com/jimbrig/PSScripts/blob/main/LICENSE
+
+.PROJECTURI https://github.com/jimbrig/PSScripts/Get-FileHash/
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+Initial Release
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Retrieve the MD5 Hashsum for a given file and it's contents. 
+
+#> 
+Param()
+
+
---
+++ Get-FileHash/README.md
@@ -0,0 +1,16 @@
+# Get-FileHash
+
+[![PowerShell Gallery Version](https://img.shields.io/powershellgallery/v/=0092ff&label=PowerShell%20Gallery&logoColor=0092ff)](https://www.powershellgallery.com/packages/Get-FileHash/1.0.0)
+
+> Retrieve the MD5 Hashsum for a given file and it's contents.
+
+## Tags
+
+Files, MD5, Hash, Serialization, Utility
+
+## Release Notes
+
+### Version 1.0.0
+
+- Initial Release
+
