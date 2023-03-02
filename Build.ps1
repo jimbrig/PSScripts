cccede1078a6b2738b5d11972f51b49f409c3129
Jimmy Briggs <jimmy.briggs@pwc.com>
2023-03-02 11:17:33 -0500

    feat: add signature injection to the build script

--- Build.ps1
+++ Build.ps1
@@ -1,15 +1,17 @@
-$dirs = (Get-ChildItem -Path $PSScriptRoot -Directory -Recurse -Exclude @('.utils', '.github', '.git', '.LocalRegistry', 'workflows')).Name
+$dirs = (Get-ChildItem -Path $PWD -Directory -Recurse -Exclude @('.utils', 'ScriptCertificates', '.github', '.git', '.LocalRegistry', 'workflows')).Name
 $scriptFiles = ForEach ($dir in $dirs) { ".\$dir\$dir.ps1" }  # $dirs | ForEach-Object { Get-ChildItem -Path "$PSScriptRoot\$_" -Filter *.ps1 -Recurse }
 
-$localRegistry = ".\.LocalRegistry"
+$localRegistry = '.\.LocalRegistry'
+
+. "$PWD\.utils\Set-ScriptSignature.ps1"
 
 If ($scriptFiles.Count -eq 0) {
-    Write-Warning "No script files found."
+    Write-Warning 'No script files found.'
     return
 }
 
 If ($null -eq $Env:NUGET_API_KEY) {
-    Write-Warning "NUGET_API_KEY environment variable not set."
+    Write-Warning 'NUGET_API_KEY environment variable not set.'
     return
 }
 
@@ -23,6 +25,7 @@ If (!(Get-PackageSource -Name LocalRegistry -ErrorAction SilentlyContinue)) {
 
 ForEach ($scriptFile in $scriptFiles) {
     Test-ScriptFileInfo -Path $scriptFile
+    Set-ScriptSignature -ScriptPath $scriptFile
     Publish-Script -Path $scriptFile -Repository LocalRegistry -NuGetApiKey $Env:NUGET_API_KEY -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
     Publish-Script -Path $scriptFile -NuGetApiKey $Env:NUGET_API_KEY -Verbose -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 }
